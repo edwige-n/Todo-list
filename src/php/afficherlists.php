1832a4044a95e13a5dc7c18a5c95b67fe3db4bc5
marionaverty <marionaverty38@gmail.com>
2022-05-26 20:04:43 +0200

    ajout bdd

--- src/php/afficherlists.php
+++ src/php/afficherlists.php
@@ -1,6 +1,5 @@
 <?php
 include 'header.php'; 
- 
 
 $query = $pdo->prepare("SELECT idListe, nomListe, DescriptionListe FROM liste"); 
 $query->execute(); 
@@ -9,8 +8,26 @@ $resultats = $query->fetchAll();
 
 $results["idListe"]["nomListe"]["DescriptionListe"] = $resultats; 
 
-retour_json(true, "Voici vos listes", $results); 
+retour_json(true, "Voici les listes", $results); 
+if( !empty($_GET['listname']) ){
+	$query = $pdo->prepare("SELECT * FROM liste WHERE nomListe LIKE :nom");
+	$query->bindParam(':nom', $_GET['listname']);
+} else {
+	$query = $pdo->prepare("SELECT * FROM liste");
+}
 
-
-
-?>
+   if ($query->execute()) { 
+       $success = true;
+       $msg = "Voici les listes :"; 
+       $liste = array(); 
+       foreach($query->fetchAll(PDO::FETCH_ASSOC) as $ligne) {
+        $liste = $ligne; 
+        $results = $liste; 
+        var_dump($results);  
+       }
+    }   
+   else {
+       $success = false; 
+       $msg = "Il y un problème...";
+   } 
+retour_json($success, $msg, $results);
